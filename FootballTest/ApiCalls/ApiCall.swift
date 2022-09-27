ad31d0fa3eee665b34dde90f13cf9948cffc0e9d
olker159 <110410058+olker159@users.noreply.github.com>
2022-09-27 20:01:01 +0200

    Update ApiCall.swift

--- FootballTest/ApiCalls/ApiCall.swift
+++ FootballTest/ApiCalls/ApiCall.swift
@@ -22,7 +22,7 @@ class ViewModel: ObservableObject {
     func fecth() {
         
         let headers = [
-            "X-RapidAPI-Key": "41b3ea560cmsh559d6da4a23e211p1ac346jsnf1a85f6123ee",
+            "X-RapidAPI-Key": "API KEY FROM RAPIDAPI.COM",
             "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
         ]
 
