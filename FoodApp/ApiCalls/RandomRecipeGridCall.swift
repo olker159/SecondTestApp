28b5314a8a21a743e069741ad9477cd2271a1444
Oliver Pavlovský <110410058+olker159@users.noreply.github.com>
2022-10-06 22:36:04 +0200

    Update RandomRecipeGridCall.swift

--- FoodApp/ApiCalls/RandomRecipeGridCall.swift
+++ FoodApp/ApiCalls/RandomRecipeGridCall.swift
@@ -94,7 +94,7 @@ class RandomRecipeGridViewModel: ObservableObject{
     func fetchRandomRecipe(){
         
         let headers = [
-            "X-RapidAPI-Key": "41b3ea560cmsh559d6da4a23e211p1ac346jsnf1a85f6123ee",
+            "X-RapidAPI-Key": "API KEY FROM RAPIDAPI.COM",
             "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
         ]
 
